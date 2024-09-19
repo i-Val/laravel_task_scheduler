1b298478222df650037de0f62cae9f0bc8570c58
i-val <ferdinantino1992@gmail.com>
2024-09-19 13:27:16 +0100

    initial commit

--- app/Models/ApiLog.php
+++ app/Models/ApiLog.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class ApiLog extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'location',
+        'response',
+        'cached_at'
+    ];
 }
